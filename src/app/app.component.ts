bdb211fdee580934406877b81d7a0bdeebd7a24d
Oseas <ozzyoliv@gmail.com>
2025-04-24 17:41:02 -0300

    feat: add animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,40 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition(':enter', [animate('0.6s ease-in')]),
+    ]),
+    trigger('slideIn', [
+      state(
+        'void',
+        style({
+          transform: 'translateY(30px)',
+          opacity: 0,
+        })
+      ),
+      transition(':enter', [animate('0.8s ease-out')]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,13 +3,35 @@ import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ProjectComponent } from './project/project.component';
 import { FooterComponent } from '../footer/footer.component';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main',
-  imports: [AboutComponent, ExperienceComponent, ProjectComponent, FooterComponent],
+  imports: [
+    AboutComponent,
+    ExperienceComponent,
+    ProjectComponent,
+    FooterComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  animations: [
+    trigger('fadeInUp', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+          transform: 'translateY(20px)',
+        })
+      ),
+      transition(':enter', [animate('0.8s {{delay}}ms ease-out')]),
+    ]),
+  ],
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
