b15606ab647ea079f79d3ff40b99ca14c2a7c502
Oseas Oliveira <ozzyoliv@gmail.com>
2026-02-11 13:39:39 -0300

    feat: add project

--- src/app/components/main/project/project.component.ts
+++ src/app/components/main/project/project.component.ts
@@ -25,6 +25,15 @@ export class ProjectComponent {
   faGithub = faGithub;
 
   projects: Project[] = [
+        {
+      id: 1,
+      title: 'Mentalaize',
+      description:
+        'A Mentalaize é uma plataforma de gestão de risco psicológico para empresas que utiliza inteligência artificial para monitorar, analisar e promover a saúde emocional dos colaboradores.',
+      image: 'assets/projects/mentalaize.png',
+      technologies: ['Ruby On Rails', 'Tailwind', 'IA', 'Typebot'],
+      githubLink: 'https://mentalaize.com.br/',
+    },
     {
       id: 2,
       title: 'OP:Memory Game',
@@ -32,7 +41,7 @@ export class ProjectComponent {
         'Este é um jogo da memória envolvendo os adorados personagens da série One Piece',
       image: 'assets/projects/opmemory.png',
       technologies: ['React.js', 'CSS', 'TypeScript'],
-      githubLink: 'https://github.com/OseOliv/op-memory-game',
+      githubLink: 'https://op-memory-game.vercel.app/',
     },
     {
       id: 3,
@@ -41,7 +50,7 @@ export class ProjectComponent {
         'Um aplicativo para calcular o Índice de Massa Corpórea (IMC)',
       image: 'assets/projects/imc.png',
       technologies: ['React.js', 'CSS', 'TypeScript'],
-      githubLink: 'https://github.com/OseOliv/IMC-APP-TS',
+      githubLink: 'https://imc-app-ts.vercel.app/',
     },
   ];
 
